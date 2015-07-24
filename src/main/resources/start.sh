53eb8f6a457c992ee7ed9c4ab5322e0445638624
leech <chaaoli@gmail.com>
2015-07-24 17:32:13 +0800

    1

--- src/main/resources/start.sh
+++ src/main/resources/start.sh
@@ -1,4 +1,4 @@
-nohup java -jar exhibitor-1.5.5.jar -c zookeeper --port 2180 --zkconfigconnect 10.5.20.18:2181,10.4.17.71:2181,10.5.20.100:2181 --zkconfigzpath /opt/exhibitor > nohup.out 2>&1 &
+nohup java -jar exhibitor-1.5.5.jar -c zookeeper --port 2180 --zkconfigconnect 127.0.0.1:2181 --zkconfigzpath /opt/exhibitor > nohup.out 2>&1 &
 sleep 1
 jps=`jps -lm | grep exhibitor-1.5.5.jar`
 if [ -n "$jps" ]; then
